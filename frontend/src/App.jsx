bb21fcabc1ef4a7743210657fa790b834db181e3
Abdou00 <abdoulaye_diarra@outlook.fr>
2024-11-24 01:52:13 +0100

    Login screen API & Login API integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,12 +11,25 @@ const App = () => {
     <div>
       <Router>
         <Routes>
-          <Route path='/dashboard' exact element={ <Home /> } />
+          <Route path='/' exact element={ <Root /> } />
           <Route path='/login' exact element={<Login />} />
           <Route path='/signup' exact element={<SignUp />} />
         </Routes>
       </Router>
     </div>
+  )
+}
+
+// Define the root component to handle the initial redirect
+const Root = () => {
+  // Check if token exists in localStorage
+  const isAuthenticed = !!localStorage.getItem('token')
+
+  // Redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticed ? (
+    <Navigate to={'/dashboard'} />
+  ) : (
+    <Navigate to={'/login'} />
   )
 }
 
