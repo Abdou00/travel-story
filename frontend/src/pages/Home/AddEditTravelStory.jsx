6bdfbc23cdb280ef803b9b76c35eaf379fac79c0
Abdou00 <abdoulaye_diarra@outlook.fr>
2024-11-24 14:25:47 +0100

    Get user info API Integration & Navbar Profile Card & Travel Card Component & isFavourite update

---
+++ frontend/src/pages/Home/AddEditTravelStory.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AddEditTravelStory = ({
+  storyInfo,
+  type,
+  onClose,
+  getAllTravelStories,
+}) => {
+  return (
+    <div>
+      AddEditTravelStory
+    </div>
+  )
+}
+
+export default AddEditTravelStory
